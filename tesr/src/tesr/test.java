bf7a51e46f0658b6af4b9e6e99d021fe5f398c57
Shyam02111 <108654161+Shyam02111@users.noreply.github.com>
2022-07-04 13:27:10 +0530

    Update test.java

--- tesr/src/tesr/test.java
+++ tesr/src/tesr/test.java
@@ -5,6 +5,6 @@ public class test {
   public static void main(String[] args){
     System.out.println("test");
 
-	System.out.println("Updated");
+	System.out.println("Updated successfully");
   }
 }
