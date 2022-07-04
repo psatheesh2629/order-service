047332f73f7d4a15017bec6e5d40638ae302e51a
Prasad Raut <prasad.raut.77@gmail.com>
2022-07-04 14:53:05 +0530

    Commited Again

--- tesr/src/tesr/test.java
+++ tesr/src/tesr/test.java
@@ -5,6 +5,6 @@ public class test {
   public static void main(String[] args){
    
 	System.out.println("Updated");
-	  System.out.println("hello");
+	  System.out.println("Hello everyone");
   }
 }
