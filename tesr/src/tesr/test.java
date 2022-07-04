a82ee8644867514af4eb01b4ea86249079033938
Akhila <satkur.akhila@infosys.com>
2022-07-04 16:46:48 +0530

    First message

--- tesr/src/tesr/test.java
+++ tesr/src/tesr/test.java
@@ -4,13 +4,7 @@ public class test {
 
   public static void main(String[] args){
 
-    System.out.printl('test12345678');
-
-
-    System.out.printl('test');
-    
-    System.out.printl('Modified');
-
+    System.out.printl('test Akhila 12345 gousepeer');
 
   }
 }
