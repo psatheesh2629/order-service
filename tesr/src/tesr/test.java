c6cb8dac9bfc7dbb6e3b435e2a60bee4f9a98fdf
mahe-95 <mahesh.bobbili@infosys.com>
2022-07-05 11:12:16 +0530

    commit done

--- tesr/src/tesr/test.java
+++ tesr/src/tesr/test.java
@@ -3,8 +3,8 @@ package tesr;
 public class test {
 
   public static void main(String[] args){
-    System.out.println("test");
+    System.out.printa("test");
 
-	System.out.println("Updated successfully");
+	System.out.print("Updated successfully");
   }
 }
