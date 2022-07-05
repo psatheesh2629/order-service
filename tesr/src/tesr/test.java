79d902c708444211e86c6ae0e1a05dadeab1ce81
shruti4edu <shruti4edu@gmail.com>
2022-07-05 12:15:15 +0530

    Commit done now

--- tesr/src/tesr/test.java
+++ tesr/src/tesr/test.java
@@ -5,7 +5,7 @@ public class test {
   public static void main(String[] args){
     System.out.println("test");
 
-	System.out.println("Updated file");
+	
                 System.out.println("New line is added");
   }
 }
