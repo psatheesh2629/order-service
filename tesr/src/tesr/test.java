f169d2a44e0f32e016444fa2ab5c563d3abf8f4f
I562081 <p.satheesh@sap.com>
2022-07-22 11:47:31 +0530

    My First Commit

--- tesr/src/tesr/test.java
+++ tesr/src/tesr/test.java
@@ -3,34 +3,6 @@ package tesr;
 public class test {
 
   public static void main(String[] args){
- 
-    System.out.printa("test");
-
-	System.out.print("Updated successfully");
-
-
-
-
-
-
-	System.out.println("Updated successfully");
-	System.out.println("Updated successfully+1");
-
-
-	System.out.println("Updated");
-	System.out.println("Da2edits");
-
-    System.out.printl('test1234');
-
-
-    System.out.printl('test');
-    
-    System.out.printl('mpodified by khaja');
-
-System.out.printl('Updated by khaja');
-
-
-
- 
+ 	System.out.println("My First Commit"); 
   }
 }
